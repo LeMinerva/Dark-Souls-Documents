97b0ffe73822ac7281371d796a83f9d1df143198
Leonardo LeMinerva <LeMinerva@outlook.com>
2023-04-04 14:50:57 +0800

    struct combine

---
+++ assets/itm.js
@@ -0,0 +1,79 @@
+document.onscroll=function(){
+  document.getElementById("pagel").style.width=(window.pageYOffset/(document.body.scrollHeight-window.innerHeight))*100+"%";
+}
+window.onload=function(){
+  function geturl(urlid){
+    var reg=new RegExp("(^|&)"+urlid+"=([^&]*)(&|$)","i");
+    var r=window.location.search.substr(1).match(reg);
+    return unescape(r[2]);
+  }
+  rmk=geturl("rmk");
+  game=geturl("game");
+  type=geturl("type");
+  lines=geturl("lines");
+  title=unescape(geturl("title").replace(/\\/g,"%").replace(/\//g,"%"));
+  document.title="黑暗之魂"+[game]+[title];
+  var xmlhttp=new window.XMLHttpRequest();
+  xmlhttp.open("get","text/chn"+[game]+"/"+[type]+"_name.xml",false);
+  xmlhttp.send();
+  xmlDoc=xmlhttp.responseXML;
+  var namec=xmlDoc.getElementsByTagName("text");
+  xmlhttp.open("get","text/jap"+[game]+"/"+[type]+"_name.xml",false);
+  xmlhttp.send();
+  xmlDoc=xmlhttp.responseXML;
+  var namej=xmlDoc.getElementsByTagName("text");
+  xmlhttp.open("get","text/eng"+[game]+"/"+[type]+"_name.xml",false);
+  xmlhttp.send();
+  xmlDoc=xmlhttp.responseXML;
+  var namee=xmlDoc.getElementsByTagName("text");
+  xmlhttp.open("get","text/chn"+[game]+"/"+[type]+"_desc.xml",false);
+  xmlhttp.send();
+  xmlDoc=xmlhttp.responseXML;
+  var descc=xmlDoc.getElementsByTagName("text");
+  xmlhttp.open("get","text/jap"+[game]+"/"+[type]+"_desc.xml",false);
+  xmlhttp.send();
+  xmlDoc=xmlhttp.responseXML;
+  var descj=xmlDoc.getElementsByTagName("text");
+  xmlhttp.open("get","text/eng"+[game]+"/"+[type]+"_desc.xml",false);
+  xmlhttp.send();
+  xmlDoc=xmlhttp.responseXML;
+  var desce=xmlDoc.getElementsByTagName("text");
+  if(rmk==0){
+    document.getElementsByClassName("chn")[0].removeChild(document.getElementsByClassName("remk")[0]);
+    document.getElementsByClassName("jap")[0].removeChild(document.getElementsByClassName("remk")[0]);
+    document.getElementsByClassName("eng")[0].removeChild(document.getElementsByClassName("remk")[0]);
+  }else{
+    xmlhttp.open("get","text/chn"+[game]+"/"+[type]+"_remk.xml",false);
+    xmlhttp.send();
+    xmlDoc=xmlhttp.responseXML;
+    var remkc=xmlDoc.getElementsByTagName("text");
+    xmlhttp.open("get","text/jap"+[game]+"/"+[type]+"_remk.xml",false);
+    xmlhttp.send();
+    xmlDoc=xmlhttp.responseXML;
+    var remkj=xmlDoc.getElementsByTagName("text");
+    xmlhttp.open("get","text/eng"+[game]+"/"+[type]+"_remk.xml",false);
+    xmlhttp.send();
+    xmlDoc=xmlhttp.responseXML;
+    var remke=xmlDoc.getElementsByTagName("text");
+}
+  frm=document.getElementsByClassName("frame")[0];
+  for(var i=0;i<[lines];i++){
+    document.getElementById("content").appendChild(frm.cloneNode(true));
+    document.getElementsByClassName("icon")[i].firstChild.src="icons/"+[type]+[game]+"/"+[i]+".png";
+    document.getElementsByClassName("chn")[i].childNodes[1].innerHTML=namec[i].childNodes[0].nodeValue.replace(/(#1|#2|#3|##)/g,function($0, $1){return{"#1":'<span class="cgb">',"#2":'<span class="cgd">',"#3":"</span>","##":"<i>未使用</i>"}[$1]});
+    document.getElementsByClassName("jap")[i].childNodes[1].innerHTML=namej[i].childNodes[0].nodeValue.replace(/##/,"<i>未使用</i>");
+    document.getElementsByClassName("eng")[i].childNodes[1].innerHTML=namee[i].childNodes[0].nodeValue.replace(/##/,"<i>UNUSED</i>");
+    document.getElementsByClassName("chn")[i].childNodes[2].firstChild.innerHTML="<p>"+descc[i].childNodes[0].nodeValue.replace(/\n/g,"</p><p>").replace(/(#1|#2|#3)/g,function($0, $1){return{"#1":'<span class="cgb">',"#2":'<span class="cgd">',"#3":"</span>"}[$1]})+"</p>";
+    document.getElementsByClassName("jap")[i].childNodes[2].firstChild.innerHTML="<p>"+descj[i].childNodes[0].nodeValue.replace(/\n/g,"</p><p>")+"</p>";
+    document.getElementsByClassName("eng")[i].childNodes[2].firstChild.innerHTML="<p>"+desce[i].childNodes[0].nodeValue.replace(/\n/g,"</p><p>")+"</p>";
+    if(rmk==1){
+      document.getElementsByClassName("chn")[i].childNodes[3].innerHTML="<p>"+remkc[i].childNodes[0].nodeValue.replace(/\n/g,"</p><p>").replace(/(#1|#2|#3)/g,function($0, $1){return{"#1":'<span class="cgb">',"#2":'<span class="cgd">',"#3":"</span>"}[$1]})+"</p>";
+      document.getElementsByClassName("jap")[i].childNodes[3].innerHTML="<p>"+remkj[i].childNodes[0].nodeValue+"</p>";
+      document.getElementsByClassName("eng")[i].childNodes[3].innerHTML="<p>"+remke[i].childNodes[0].nodeValue+"</p>";
+    }
+  }
+  document.getElementById("content").removeChild(document.getElementsByClassName("frame")[i]);
+  document.getElementById([type]+[game]).setAttribute("class","mainitm");
+  document.getElementById("content").setAttribute("class","content"+" game"+[game]);
+  document.getElementsByClassName('content')[1].onclick=function(event){if(event.target.className==='top'){document.getElementsByClassName('content')[1].insertBefore(event.target.parentNode,event.target.parentNode.parentNode.firstChild)}}
+}
